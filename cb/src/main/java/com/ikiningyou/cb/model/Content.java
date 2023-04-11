581e6760bac5f31faef4eef1c6dcacda2c687c6e
Joowon <201611804@kyonggi.ac.kr>
2023-04-11 23:20:07 +0900

    update content model

--- cb/src/main/java/com/ikiningyou/cb/model/Content.java
+++ cb/src/main/java/com/ikiningyou/cb/model/Content.java
@@ -4,23 +4,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 
 @Builder
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-
 public class Content {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private String content;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String title;
+  private String content;
+  private String writer;
+  private Date created;
 }
