5b33f98090979dc734ba6be9a04ea499c50b093a
Joowon <tkdel222@gmail.com>
2023-04-21 23:28:27 +0900

    remove one to many with comment on content

--- cb/src/main/java/com/ikiningyou/cb/model/Content.java
+++ cb/src/main/java/com/ikiningyou/cb/model/Content.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
