d2c4d2e750ef09e4edd3647a7effede54885effa
Joowon <tkdel222@gmail.com>
2023-05-17 23:12:27 +0900

    update ignore csrf on register

--- cb/src/main/java/com/ikiningyou/cb/config/WebConfig.java
+++ cb/src/main/java/com/ikiningyou/cb/config/WebConfig.java
@@ -2,18 +2,14 @@ package com.ikiningyou.cb.config;
 
 import com.ikiningyou.cb.repository.CustomCsrfTokenRepo;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -37,6 +33,7 @@ public class WebConfig {
       .addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
       .httpBasic(c -> c.disable())
       .csrf(c -> {
+        c.ignoringRequestMatchers("/user/register");
         c.csrfTokenRepository(customTokenRepository());
       })
       .cors(c -> {
