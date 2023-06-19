52b48252ce205126bf3eabf9dfb396c82b7e4a33
Joowon <tkdel222@gmail.com>
2023-06-19 20:45:02 +0900

    remove unused codes

--- cb/src/test/java/com/ikiningyou/cb/ContentTest.java
+++ cb/src/test/java/com/ikiningyou/cb/ContentTest.java
@@ -1,10 +1,6 @@
 package com.ikiningyou.cb;
 
-import com.ikiningyou.cb.service.BoardService;
-import java.io.UnsupportedEncodingException;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.annotation.Testable;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
